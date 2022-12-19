624bcc42bda76a41ce563c52a6858daec23a0241
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-12-19 11:10:18 +1000

    Add usage to find books simlar to the hobbit

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import os
-from modeling.tokenizer import OpenAITokenizer
 
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+from modeling.data import loadTokenizedBookSummaries
 
 
 if __name__ == '__main__':
     
     keyVariable = 'OPENAI_API_KEY'
-    tokenizer = OpenAITokenizer(keyVariable)
-
-    example = ['This is a test','and so is this']
-
     
-    print(tokenizer.tokenize(example).shape)
+    titles, summaries = loadTokenizedBookSummaries(
+                            'bin/titles.npy',                       #Book titles
+                            'bin/tokenizedSummaries.npy',           #Tokenized summaries
+                            'bin/booksummaries/booksummaries.txt',  #Book summaries
+                            keyVariable=keyVariable                 #OpenAI API key
+                        )
+
+    #Find the most similar book to the Hobbit (85)
+    n = 85
+    similarity = cosine_similarity(summaries[n].reshape(1,-1), summaries)
+
+    #Find the 10 most similar books
+    mostSimilar = np.argsort(similarity[0])[::-1][1:11] #skip the first (will be the same book)
+
+    #Print the results
+    print('Most similar books to: ', titles[n])
+    for i in mostSimilar:
+        print(titles[i], similarity[0][i])
